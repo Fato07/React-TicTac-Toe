aca1c2f1a8bf2d39c595993413b62308232a542e
Fathin Dosunmu <fathin.dosunmu@exove.fi>
2022-08-17 12:04:47 +0300

    react router initial setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './sass/_app.scss';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import GameBoard from './pages/GameBoard';
+import ScoreBoard from './pages/ScoreBoard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="game-board" element={<GameBoard />} />
+        <Route path="score-board" element={<ScoreBoard />} />
+      </Routes>
     </div>
   );
 }
