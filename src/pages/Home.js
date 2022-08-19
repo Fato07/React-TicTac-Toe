d5206d00d4f1699afa0e21a0071c769d61859279
Sha256_Nulled <40800448+Fato07@users.noreply.github.com>
2022-08-19 12:15:26 +0300

    code refactorings and fixed Typist Bug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TicTacToeContext } from '../TicTacToeContext';
+import Typist from 'react-typist';
 
 const Home = () => {
 
@@ -13,10 +14,13 @@ const Home = () => {
 
  return (
   <>
+   <Typist className="lead" cursor={{ show: false }}>
+    <h1 className='home-title'>Welcome To A Game of TicTacToe</h1>
+   </Typist>
 
-   <h1 className='home-title'>Enter Player Names</h1>
+
    <div className='home-container'>
-
+    <h3 className='home-title'>Enter Player Names To Begin</h3>
 
     <div class="form__group">
      <input type="input" class="form__field" placeholder="Player 1" name="player1" id='player1' required onChange={(e) => setState(state => ({ ...state, player1: e.target.value }))} />
