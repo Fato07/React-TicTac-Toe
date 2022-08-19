eb8bf4ae621da020d66872aa223f3efc220095f9
Sha256_Nulled <40800448+Fato07@users.noreply.github.com>
2022-08-19 12:12:49 +0300

    added stylings for scoreBoard

--- src/pages/ScoreBoard.js
+++ src/pages/ScoreBoard.js
@@ -6,9 +6,19 @@ const ScoreBoard = () => {
  const [state, setState] = useContext(TicTacToeContext);
  return (
   <>
-   <h1>ScoreBoard</h1>
-   <p>{state.player1} : {state.xScore}</p>
-   <p>{state.player2} : {state.oScore}</p>
+   <h1 className='title'>ScoreBoard</h1>
+   <div class="scoreboard">
+    <div class="points-container">
+     <div class="points">
+      <h3>{state.player1}</h3>
+      <p id="home-points" class="px-3">{state.xScore}</p>
+     </div>
+     <div class="points ">
+      <h3>{state.player2}</h3>
+      <p id="guest-points" class="px-3">{state.oScore}</p>
+     </div>
+    </div>
+   </div>
   </>
  );
 };
