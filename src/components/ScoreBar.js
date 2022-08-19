7f0c8e110aae803ea6603ee49a2a9e0ebf565755
Sha256_Nulled <40800448+Fato07@users.noreply.github.com>
2022-08-19 12:15:07 +0300

    fixed className

--- src/components/ScoreBar.js
+++ src/components/ScoreBar.js
@@ -5,7 +5,7 @@ const ScoreBar = ({ xPlaying }) => {
  const [state, setState] = useContext(TicTacToeContext);
 
  return (
-  <div className="scoreboard">
+  <div className="scorebar">
    <span className={`score x-score ${!xPlaying && "inactive"}`}>{state.player1} - {state.xScore}</span>
    <span className={`score o-score ${xPlaying && "inactive"}`}>{state.player2} - {state.oScore}</span>
   </div>
